ac349fee8dd7e28643295f979f7c9ad92877a78b
Ansh Yadav <anshy047@gmail.com>
2026-05-23 21:21:58 +0530

    Ignore Next.js build files

--- .next/static/chunks/src_app_(dashboard)_layout_tsx_0dd78360._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_63710670._.js",
-  "static/chunks/src_f8f2a779._.js"
-],
-    source: "dynamic"
-});
